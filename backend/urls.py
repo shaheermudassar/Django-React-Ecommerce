7c6637a8a25afa3d8311fb06fe7889b729804f78
shaheermudassar <shaheer.mudassar13@gmail.com>
2023-08-18 17:06:29 +0500

    updated

--- backend/urls.py
+++ backend/urls.py
@@ -18,14 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.views.static import serve
-# from django.urls import re_path
+from django.views.static import serve
+from django.urls import re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("app.urls")),
     path('', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
